784402876ba9121ff4bc6ab1b6a383bc1c7a1d60
Dylan Grinberg <dylanpdx@gmail.com>
2019-11-06 22:48:33 +0000

    Add project files.

---
+++ BabyVolume/Baby.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BabyVolume
+{
+    partial class Baby
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pollTimer = new System.Windows.Forms.Timer(this.components);
+            this.VolText = new System.Windows.Forms.Label();
+            this.TextTimeoutTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BabyVolume.Properties.Resources._00;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(335, 338);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // pollTimer
+            // 
+            this.pollTimer.Enabled = true;
+            this.pollTimer.Interval = 1;
+            this.pollTimer.Tick += new System.EventHandler(this.pollTimer_Tick);
+            // 
+            // VolText
+            // 
+            this.VolText.AutoSize = true;
+            this.VolText.BackColor = System.Drawing.Color.White;
+            this.VolText.Location = new System.Drawing.Point(12, 316);
+            this.VolText.Name = "VolText";
+            this.VolText.Size = new System.Drawing.Size(86, 13);
+            this.VolText.TabIndex = 1;
+            this.VolText.Text = "Sensitivity: 100%";
+            this.VolText.Visible = false;
+            // 
+            // TextTimeoutTimer
+            // 
+            this.TextTimeoutTimer.Interval = 3000;
+            this.TextTimeoutTimer.Tick += new System.EventHandler(this.TextTimeoutTimer_Tick);
+            // 
+            // Baby
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(335, 338);
+            this.Controls.Add(this.VolText);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = global::BabyVolume.Properties.Resources.baby;
+            this.Name = "Baby";
+            this.Text = "Baby";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Baby_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer pollTimer;
+        private System.Windows.Forms.Label VolText;
+        private System.Windows.Forms.Timer TextTimeoutTimer;
+    }
+}
+
---
+++ BabyVolume/Baby.cs
@@ -0,0 +1,136 @@
+﻿using BabyVolume.Properties;
+using CSCore.CoreAudioAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BabyVolume
+{
+    public partial class Baby : Form
+    {
+        static Image[] babyFaces = new Image[] { Resources._00, Resources._01 , Resources._02 , Resources._03 ,
+            Resources._04, Resources._05, Resources._06, Resources._07, Resources._08, Resources._09,Resources._10,
+        Resources._11,Resources._12,Resources._13,Resources._14,Resources._15,Resources._16,Resources._17};
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        static AudioSessionManager2 sessmgr;
+        static AudioSessionEnumerator sessenum;
+
+        float thresh = 100;
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        public Baby()
+        {
+            InitializeComponent();
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private static float getHighestVol()
+        {
+            if (sessmgr == null)
+                return 0;
+            if (sessenum == null)
+                sessenum = sessmgr.GetSessionEnumerator();
+            float highest = 0;
+            foreach (var session in sessenum)
+            {
+                using (var audioMeterInformation = session.QueryInterface<AudioMeterInformation>())
+                {
+                    float peak = audioMeterInformation.GetPeakValue();
+                    if (peak > highest)
+                        highest = peak;
+                }
+            }
+
+            return highest;
+        }
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager2(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    var sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    return sessionManager;
+                }
+            }
+        }
+
+        private void pollTimer_Tick(object sender, EventArgs e)
+        {
+            float highest = getHighestVol();
+            highest *= (thresh/100);
+            int babyIndex = (int)(highest * (babyFaces.Length - 1));
+            if (babyIndex > babyFaces.Length - 1)
+                babyIndex = babyFaces.Length - 1;
+            pictureBox1.Image = babyFaces[babyIndex];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                sessmgr = GetDefaultAudioSessionManager2(DataFlow.Render);
+            }).Start();
+        }
+
+        private void Baby_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                TextTimeoutTimer.Stop();
+                TextTimeoutTimer.Start();
+                VolText.Visible = true;
+                thresh += 10f;
+                thresh = Clamp(thresh,50, 200);
+                VolText.Text = "Sensitivity: " + ((int)thresh)+"%";
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                TextTimeoutTimer.Stop();
+                TextTimeoutTimer.Start();
+                VolText.Visible = true;
+                thresh -= 10f;
+                thresh = Clamp(thresh, 50, 200);
+                VolText.Text = "Sensitivity: " + ((int)thresh) + "%";
+            }else if (e.KeyCode == Keys.Space)
+            {
+                TextTimeoutTimer.Stop();
+                TextTimeoutTimer.Start();
+                VolText.Text = "by dylanpdx#5558";
+            }
+        }
+
+        private void TextTimeoutTimer_Tick(object sender, EventArgs e)
+        {
+            VolText.Visible = false;
+        }
+    }
+}
